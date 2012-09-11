020bacb2e23f0281f9f1ca1928247c4114185913
Christopher Thielen <cthielen@gmail.com>
2012-09-11 12:50:02 -0700

    Merged commit from git://github.com/garygreen/unserialize-to-form.git master. Thanks Gary Green

--- jQuery.unserializeForm.js
+++ jQuery.unserializeForm.js
@@ -1,5 +1,5 @@
 // Unserialize (to) form plugin
-// Version 1.0.7
+// Version 1.0.8
 // Copyright (C) 2010-2012 Christopher Thielen, others (see ChangeLog below)
 // Dual-licensed under GPLv2 and the MIT open source licenses
 
@@ -67,7 +67,7 @@
      		}
      	}
     },
-    
+
     _pushValue : function( obj, key, val ) {
       if (null == obj[key])
         obj[key] = val;
@@ -77,45 +77,45 @@
         obj[key] = [obj[key], val];
     }
   };
-  
+
 	// takes a GET-serialized string, e.g. first=5&second=3&a=b and sets input tags (e.g. input name="first") to their values (e.g. 5)
 	$.fn.unserializeForm = function( _values, _options ) {
-	  
+
 	  // Set up defaults
     var settings = $.extend( {
       'callback'         : undefined,
       'override-values'  : false
     }, _options);
-	  
+
 	  return this.each(function() {
   		// this small bit of unserializing borrowed from James Campbell's "JQuery Unserialize v1.0"
   		_values = _values.split("&");
   		_callback = settings["callback"];
   		_override_values = settings["override-values"];
-		
+
   		if(_callback && typeof(_callback) !== "function") {
   			_callback = undefined; // whatever they gave us wasn't a function, act as though it wasn't given
   		}
-		
+
   		var serialized_values = new Array();
   		$.each(_values, function() {
   			var properties = this.split("=");
-			
+
   			if((typeof properties[0] != 'undefined') && (typeof properties[1] != 'undefined')) {
           methods._pushValue(serialized_values, properties[0].replace(/\+/g, " "), decodeURI(properties[1].replace(/\+/g, " ")));
   			}
   		});
-		
+
   		// _values is now a proper array with values[hash_index] = associated_value
   		_values = serialized_values;
-      
+
   		// Start with all checkboxes and radios unchecked, since an unchecked box will not show up in the serialized form
   		$(this).find(":checked").attr("checked", false);
-		
+
   		// Iterate through each saved element and set the corresponding element
   		for(var key in _values) {
   			var el = $(this).add("input,select,textarea").find("[name=\"" + unescape(key) + "\"]");
-        
+
         if(typeof(_values[key]) != "string") {
           // select tags using 'multiple' will be arrays here (reports "object")
           // We cannot do the simple unescape() because it will flatten the array.
@@ -127,14 +127,14 @@
         } else {
   			  var _value = unescape(_values[key]);
         }
-			  
+
   			if(_callback == undefined) {
   				// No callback specified - assume DOM elements exist
   				methods._unserializeFormSetValue(el, _value, _override_values);
   			} else {
   				// Callback specified - don't assume DOM elements already exist
   				var result = _callback.call(this, unescape(key), _value, el);
-          
+
   				// If they return true, it means they handled it. If not, we will handle it.
   				// Returning false then allows for DOM building without setting values.
   				if(result == false) {
@@ -172,3 +172,6 @@
 //                                    * Added queried element (if found) as an optional third parameter for callbacks
 // 2012-02-29: Version 1.0.7 release:
 //                                    * Added support for <select multiple>. Thanks Joshua Weinstein
+// 2012-09-11: Version 1.0.8 release:
+//                                    * Added support for input element arrays. Thanks Gary Green.
+//                                      Note input element arrays should first be cleared.
